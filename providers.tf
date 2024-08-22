2a5b669a8f4e320ac6fe775203bad49887508c17
safwenketata <81825117+safwenketata@users.noreply.github.com>
2024-08-22 15:55:53 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,13 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "aks_tf_rg"
+    storage_account_name = "looyastorage"
+    container_name       = "looyas"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
